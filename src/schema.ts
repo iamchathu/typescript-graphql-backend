d16e7f79d996f3b6f4ea4a4398ffd90e85829cef
Chathu Vishwajith <4evercreations@gmail.com>
2020-05-26 00:03:07 +0530

    Refactor Book year data type to Int

--- src/schema.ts
+++ src/schema.ts
@@ -1,17 +1,34 @@
 import { gql } from 'apollo-server-express';
 
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+    year: 1998,
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+    year: 1990,
+  },
+];
+
 export const typeDefs = gql`
   # Type definition for Book
-
+  type Book {
+    title: String
+    author: String
+    year: Int
+  }
   # Type definition of resolvers
   type Query {
-    hello: String
+    hello: [Book]
   }
 `;
 
 export const resolvers = {
   // Resolver implementation
   Query: {
-    hello: () => 'Hello WTM Manila',
+    books: () => books,
   },
 };
