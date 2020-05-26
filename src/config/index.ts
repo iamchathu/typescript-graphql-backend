2ea21c4f7a688cee878d970512cf3d07bb6e8ec6
Chathu Vishwajith <4evercreations@gmail.com>
2020-05-26 11:44:06 +0530

    Fix default name database name in config for local database

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ const config = {
     uri:
       process.env.MONGO_LAB_URL ||
       process.env.MONGODB_URI ||
-      `mongodb://${process.env.DB_PORT_TCP_ADDR || 'localhost'}/sync-db`,
+      `mongodb://${process.env.DB_PORT_TCP_ADDR || 'localhost'}/books-db`,
     options: {
       useNewUrlParser: true,
       keepAlive: true,
